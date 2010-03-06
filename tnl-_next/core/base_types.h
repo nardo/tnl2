0d3da0f09166bba180b9491589dcaccdbe0adb0f
Mark Frohnmayer <mark.frohnmayer@gmail.com>
2010-03-05 18:13:12 -0800

    cleanup

--- tnl-_next/core/base_types.h
+++
@@ -1,2 +0,0 @@
-// Copyright GarageGames.  See /license/info.txt in this distribution for licensing terms.
-// basic type declarations; due to namespace ambiguity errors, these exist in the global namespace.
--- tnl-_next/crypto/crypto.h
+++
@@ -1,8 +0,0 @@
-/// crypto files
-
-#include "random_generator.h"
-#include "symmetric_cipher.h"
-//#include "asymmetric_key.h"
-//#include "certificate.h"
-
-
